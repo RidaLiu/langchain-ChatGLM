be6233bd11ba8934421d3b13762ffba021988f61
Rida Liu <gojnt@WCNT017292.bayer.cnb>
2023-06-08 17:32:14 +0800

    accept the azure api

--- models/fastchat_openai_llm.py
+++ models/fastchat_openai_llm.py
@@ -1,15 +1,12 @@
 from abc import ABC
+import os
 import requests
 from typing import Optional, List
 from langchain.llms.base import LLM
 
 from models.loader import LoaderCheckPoint
-from models.base import (RemoteRpcModel,
-                         AnswerResult)
-from typing import (
-    Collection,
-    Dict
-)
+from models.base import RemoteRpcModel, AnswerResult
+from typing import Collection, Dict
 
 
 def _build_message_template() -> Dict[str, str]:
@@ -74,46 +71,64 @@ class FastChatOpenAILLM(RemoteRpcModel, LLM, ABC):
     # 将历史对话数组转换为文本格式
     def build_message_list(self, query) -> Collection[Dict[str, str]]:
         build_message_list: Collection[Dict[str, str]] = []
-        history = self.history[-self.history_len:] if self.history_len > 0 else []
+        history = (
+            self.history[-self.history_len :] if self.history_len > 0 else []
+        )
         for i, (old_query, response) in enumerate(history):
             user_build_message = _build_message_template()
-            user_build_message['role'] = 'user'
-            user_build_message['content'] = old_query
+            user_build_message["role"] = "user"
+            user_build_message["content"] = old_query
             system_build_message = _build_message_template()
-            system_build_message['role'] = 'system'
-            system_build_message['content'] = response
+            system_build_message["role"] = "system"
+            system_build_message["content"] = response
             build_message_list.append(user_build_message)
             build_message_list.append(system_build_message)
 
         user_build_message = _build_message_template()
-        user_build_message['role'] = 'user'
-        user_build_message['content'] = query
+        user_build_message["role"] = "user"
+        user_build_message["content"] = query
         build_message_list.append(user_build_message)
         return build_message_list
 
-    def generatorAnswer(self, prompt: str,
-                        history: List[List[str]] = [],
-                        streaming: bool = False):
-
+    def generatorAnswer(
+        self,
+        prompt: str,
+        history: List[List[str]] = [],
+        streaming: bool = False,
+    ):
         try:
             import openai
+
             # Not support yet
-            openai.api_key = "EMPTY"
-            openai.api_base = self.api_base_url
+            if os.getenv("CALL_AZURE_OPENAI") == "True":
+                openai.api_type = "azure"
+                openai.api_base = os.getenv("AZURE_OPENAI_ENDPOINT")
+                openai.api_version = os.getenv("AZURE_OPENAI_VERSION")
+                openai.api_key = os.getenv("AZURE_OPENAI_KEY")
+                completion = openai.ChatCompletion.create(
+                    engine=os.getenv("AZURE_OPENAI_ENGINE"),
+                    model=self.model_name,
+                    messages=self.build_message_list(prompt),
+                )
+            else:
+                openai.api_key = "EMPTY"
+                openai.api_base = self.api_base_url
+                # create a chat completion
+                completion = openai.ChatCompletion.create(
+                    model=self.model_name,
+                    messages=self.build_message_list(prompt),
+                )
         except ImportError:
             raise ValueError(
                 "Could not import openai python package. "
                 "Please install it with `pip install openai`."
             )
-        # create a chat completion
-        completion = openai.ChatCompletion.create(
-            model=self.model_name,
-            messages=self.build_message_list(prompt)
-        )
 
         history += [[prompt, completion.choices[0].message.content]]
         answer_result = AnswerResult()
         answer_result.history = history
-        answer_result.llm_output = {"answer": completion.choices[0].message.content}
+        answer_result.llm_output = {
+            "answer": completion.choices[0].message.content
+        }
 
         yield answer_result
